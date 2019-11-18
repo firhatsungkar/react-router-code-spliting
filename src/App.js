19a40f6028b63f82d89e1e2b0409720dcdeffcd6
Muhamad Firhat <muhamad.firhat@gmail.com>
2019-11-18 10:40:25 +0700

    learn server render

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect,
 } from 'react-router-dom';
 
 import Home from './views/Home'
@@ -13,6 +14,32 @@ import Users from './views/Users'
 // import logo from './logo.svg';
 import './App.css';
 import Topics from './views/Topics';
+
+const RedirectWithStatus = ({ from, to, status}) => (
+  <Route
+    render={({ staticContext }) => {
+      if (staticContext) staticContext.status = status
+      return <Redirect from={from} to={to} />
+    }}
+  />
+)
+
+const Status = ({ code, children }) => (
+  <Route
+    render={({ staticContext }) => {
+      if (staticContext) staticContext.status = code
+      return children
+    }}
+  />
+)
+
+const NotFound = () => (
+  <Status code={404}>
+    <div>
+      <h1>Sorry can't find that.</h1>
+    </div>
+  </Status>
+)
 
 function App() {
   return (
@@ -30,18 +57,26 @@ function App() {
               <li>
                 <Link className="App-link" to="/topics">Topics</Link>
               </li>
+              <li>
+                <Link className="App-link" to="/profile">Profile</Link>
+              </li>
             </ul>
           </nav>
           <Switch>
             <Route path="/about">
               <About />
             </Route>
+            <Route path="/users">
+              <Users/>
+            </Route>
             <Route path="/topics">
               <Topics/>
             </Route>
-            <Route path="/">
+            <RedirectWithStatus status={301} from="/profile" to="/users"/>
+            <Route path="/" exact>
               <Home/>
             </Route>
+            <Route component={NotFound}/>
           </Switch>
         </header>
       </div>
--- src/views/Topics.js
+++ src/views/Topics.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {
   useRouteMatch,
-  Link,
+  // Link,
   Switch,
   Route,
   NavLink,
